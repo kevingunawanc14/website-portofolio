33db3776c4925a1b52d8c11c2eeb09436c772fad
kevingunawanc14 <c14200005@john.petra.ac.id>
2024-11-09 13:03:52 +0700

    feat: change footer ui

--- src/pages/bible/components/footer.tsx
+++ src/pages/bible/components/footer.tsx
@@ -3,19 +3,94 @@ import React from 'react'
 function Footer() {
     return (
         <div className='container '>
-            <div className='h-40 grid grid-cols-3 content-end pb-5 '>
-                <div>
-                    <a href="https://lordicon.com/" className='poppins-medium text-sm'>Animated icons by Lordicon.com
-                    </a>
+            <div className='h-40 grid grid-cols-1 sm:grid-cols-3 content-end pb-1  '>
+
+                <div className='border-t-2'>
+                    <p className='poppins-medium  text-sm mt-2 text-center '>Community</p>
+                    <div className='flex justify-center cursor-pointer'>
+                        <lord-icon
+                            src="https://cdn.lordicon.com/zvnxzuwv.json"
+                            trigger="loop"
+                            style={{
+                                // height: '2rem',
+                                paddingTop: '5px'
+                            }}
+                        >
+                        </lord-icon>
+                    </div>
+
+
+                </div>
+                <div className='border-t-2'>
+                    <p className='poppins-medium  text-sm mt-2 text-center'>Support</p>
+                    <div className='flex justify-center cursor-pointer' >
+                        <lord-icon
+                            src="https://cdn.lordicon.com/jdgfsfzr.json"
+                            trigger="loop"
+                            style={{
+                                // height: '2rem',
+                                paddingTop: '5px'
+                            }}
+                        >
+                        </lord-icon>
+                    </div>
+
+                </div>
+                {/* <div className='border-t-2'>
+                    <p className='poppins-medium  text-sm mt-2 text-center'>Feature Requests</p>
+                    <div className='flex justify-center cursor-pointer'>
+                        <lord-icon
+                            src="https://cdn.lordicon.com/pflszboa.json"
+                            trigger="loop"
+                            style={{
+                                // height: '2rem',
+                                paddingTop: '5px'
+                            }}
+                        >
+                        </lord-icon>
+                    </div>
+
+                </div>
+                <div className='border-t-2'>
+                    <p className='poppins-medium  text-sm mt-2 text-center'>Bug Report</p>
+                    <div className='flex justify-center cursor-pointer'>
+                        <lord-icon
+                            src="https://cdn.lordicon.com/rpgflpkp.json"
+                            trigger="loop"
+                            state="loop-bug"
+                            style={{
+                                // height: '2rem',
+                                paddingTop: '5px'
+                            }}
+                        >
+                        </lord-icon>
+                    </div>
+
+                </div> */}
+                <div className='border-t-2 text-center'>
+                    <div className='mt-1'>
+                        <a href="https://lordicon.com/" className='poppins-medium text-sm   '>Animated icons by Lordicon.com
+                        </a>
+                        <div className='flex justify-center cursor-pointer'>
+                            <lord-icon
+                                src="https://cdn.lordicon.com/yrzvvdtr.json"
+                                trigger="loop"
+                                style={{
+                                    // height: '2rem',
+                                    paddingTop: '5px'
+                                }}
+                            >
+                            </lord-icon>
+                        </div>
+
+                    </div>
                 </div>
 
-                <div>
 
-                </div>
 
             </div>
 
-        </div>
+        </div >
     )
 }
 
