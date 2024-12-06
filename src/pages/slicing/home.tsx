7457c70544b6fe6228c369130c5a6c855a67c5d4
kevingunawanc14 <c14200005@john.petra.ac.id>
2024-12-06 13:31:06 +0700

    fix: responsive product and service

--- src/pages/slicing/home.tsx
+++ src/pages/slicing/home.tsx
@@ -116,16 +116,33 @@ function Home() {
           <div>
             <div className='grid grid-col-1 md:grid-cols-3 mt-9 gap-4 md:gap-10 lg:gap-28 '>
               <div className='flex justify-center'>
-                <a href="#" className="block sm:w-[200px] sm:h-[250px]  md:w-[350px]  md:h-[400px] p-6 bg-slicing-foreground border rounded shadow pt-10">
+                <div className=" bg-slicing-foreground border rounded shadow-md p-10">
                   <div className='flex justify-center'>
                     <img src="/images/slicing/serviceChart.png" alt="" />
-
                   </div>
-                  <p className='text-[24px] poppins-bold text-center text-white my-6  '>Harga Terjangkau</p>
-                  <p className='text-[18px] poppins-regular text-center text-white'>Peralatan berkualitas tak harus mahal. Dengan harga yang terjangkau, kamu bisa menikmati  peralatan outdoor berkualitas dari kami.</p>
-                </a>
+                  <p className='text-[24px] sm:text-[16px]  xl:text-[24px] poppins-bold text-center text-white my-6   '>Harga Terjangkau</p>
+                  <p className='text-[14px] sm:text-[12px]  xl:text-[18px]  poppins-regular text-center text-white'>Peralatan berkualitas tak harus mahal. Dengan harga yang terjangkau, kamu bisa menikmati  peralatan outdoor berkualitas dari kami.</p>
+                </div>
               </div>
               <div className='flex justify-center'>
+                <div className=" bg-slicing-foreground border rounded shadow-md p-10">
+                  <div className='flex justify-center'>
+                    <img src="/images/slicing/serviceTruck.png" alt="" />
+                  </div>
+                  <p className='text-[24px] sm:text-[16px]  xl:text-[24px] poppins-bold text-center text-white my-6   '>Gratis Antar Jemput</p>
+                  <p className='text-[14px] sm:text-[12px]  xl:text-[18px]  poppins-regular text-center text-white'>Tidak perlu repot mengangkut peralatan mendaki. Manfaatkan layanan antar jemput gratis kami dan fokuslah pada petualanganmu.</p>
+                </div>
+              </div>
+              <div className='flex justify-center'>
+                <div className=" bg-slicing-foreground border rounded shadow-md p-10">
+                  <div className='flex justify-center'>
+                    <img src="/images/slicing/serviceCamp.png" alt="" />
+                  </div>
+                  <p className='text-[24px] sm:text-[16px]  xl:text-[24px] poppins-bold text-center text-white my-6   '>Peralatan Berkualitas</p>
+                  <p className='text-[14px] sm:text-[12px]  xl:text-[18px]  poppins-regular text-center text-white'>Kami menyediakan berbagai peralatan berkualitas untuk memastikan keamanan dan kenyamananmu selama berpetualang.</p>
+                </div>
+              </div>
+              {/* <div className='flex justify-center'>
                 <a href="#" className="block  sm:w-[200px] sm:h-[250px]  md:w-[350px]  md:h-[400px] p-6 bg-slicing-foreground border rounded shadow pt-10">
                   <div className='flex justify-center'>
                     <img src="/images/slicing/serviceTruck.png" alt="" />
@@ -144,7 +161,7 @@ function Home() {
                   <p className='text-[24px] poppins-bold text-center text-white my-6  '>Peralatan Berkualitas</p>
                   <p className='text-[18px] poppins-regular text-center text-white'>Kami menyediakan berbagai peralatan berkualitas untuk memastikan keamanan dan kenyamananmu selama berpetualang.</p>
                 </a>
-              </div>
+              </div> */}
             </div>
           </div>
         </div>
@@ -157,93 +174,72 @@ function Home() {
             <p className='poppins-bold text-[26px] sm:text-[28px] md:text-[40px] text-center mt-3 md:mt-[20px]'>Peralatan ini mungkin akan membantumu dalam berpetualang</p>
           </div>
           <div>
-            <div className='grid grid-cols-2 md:grid-cols-4 mt-9 gap-5'>
+            <div className='grid grid-cols-2 md:grid-cols-4 mt-9 gap-[29px] '>
               <div>
-                <div className="max-w-sm bg-white border border-gray-200 rounded shadow">
-                  <a href="#">
-                    <img className="rounded-t bg-slicing-deep" src="/images/slicing/sleepingBag.png" alt="" />
-                  </a>
+                <div className="shadow-md rounded    ">
+                  <img className=" bg-slicing-deep rounded-t" src="/images/slicing/sleepingBag.png" alt="" />
                   <div className="p-5">
-
-                    <p className='text-[20px] poppins-bold'>Sleeping Bag</p>
-                    <p className="text-[18px] text-slicing-grey poppins-regular mt-2">Rp. 10.000 <span className='text-end'> / hari</span></p>
+                    <p className='text-[12px] lg:text-[14px]  xl:text-[20px] poppins-bold truncate'>Sleeping Bag</p>
+                    <p className="text-[12px] lg:text-[12px]  xl:text-[18px] text-slicing-grey poppins-regular mt-2">Rp. 10.000 <span className='text-end'> / hari</span></p>
                     <div className='flex justify-center mt-2'>
                       <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M17 18C15.89 18 15 18.89 15 20C15 20.5304 15.2107 21.0391 15.5858 21.4142C15.9609 21.7893 16.4696 22 17 22C17.5304 22 18.0391 21.7893 18.4142 21.4142C18.7893 21.0391 19 20.5304 19 20C19 19.4696 18.7893 18.9609 18.4142 18.5858C18.0391 18.2107 17.5304 18 17 18ZM1 2V4H3L6.6 11.59L5.24 14.04C5.09 14.32 5 14.65 5 15C5 15.5304 5.21071 16.0391 5.58579 16.4142C5.96086 16.7893 6.46957 17 7 17H19V15H7.42C7.3537 15 7.29011 14.9737 7.24322 14.9268C7.19634 14.8799 7.17 14.8163 7.17 14.75C7.17 14.7 7.18 14.66 7.2 14.63L8.1 13H15.55C16.3 13 16.96 12.58 17.3 11.97L20.88 5.5C20.95 5.34 21 5.17 21 5C21 4.73478 20.8946 4.48043 20.7071 4.29289C20.5196 4.10536 20.2652 4 20 4H5.21L4.27 2M7 18C5.89 18 5 18.89 5 20C5 20.5304 5.21071 21.0391 5.58579 21.4142C5.96086 21.7893 6.46957 22 7 22C7.53043 22 8.03914 21.7893 8.41421 21.4142C8.78929 21.0391 9 20.5304 9 20C9 19.4696 8.78929 18.9609 8.41421 18.5858C8.03914 18.2107 7.53043 18 7 18Z" fill="#156E3E" />
                       </svg>
-                      <p className="ms-3 text-[18px] text-slicing-foreground poppins-bold">
+                      <p className="ms-3 text-[10px] lg:text-[12px]  xl:text-[18px]  text-slicing-foreground poppins-bold">
 
                         Masukan Keranjang</p>
                     </div>
-
-
-                  </div>
-                </div>
-
-              </div>
-              <div>
-                <div className="max-w-sm bg-white border border-gray-200 rounded shadow">
-                  <a href="#">
-                    <img className="rounded-t bg-slicing-deep" src="/images/slicing/redBag.png" alt="" />
-                  </a>
-                  <div className="p-5">
-
-                    <p className='text-[20px] poppins-bold'>Carrier 60L + Rain Cover</p>
-                    <p className="text-[18px] text-slicing-grey poppins-regular mt-2">Rp 25.000 <span className='text-end'> / hari</span></p>
-                    <div className='flex justify-center mt-2'>
-                      <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M17 18C15.89 18 15 18.89 15 20C15 20.5304 15.2107 21.0391 15.5858 21.4142C15.9609 21.7893 16.4696 22 17 22C17.5304 22 18.0391 21.7893 18.4142 21.4142C18.7893 21.0391 19 20.5304 19 20C19 19.4696 18.7893 18.9609 18.4142 18.5858C18.0391 18.2107 17.5304 18 17 18ZM1 2V4H3L6.6 11.59L5.24 14.04C5.09 14.32 5 14.65 5 15C5 15.5304 5.21071 16.0391 5.58579 16.4142C5.96086 16.7893 6.46957 17 7 17H19V15H7.42C7.3537 15 7.29011 14.9737 7.24322 14.9268C7.19634 14.8799 7.17 14.8163 7.17 14.75C7.17 14.7 7.18 14.66 7.2 14.63L8.1 13H15.55C16.3 13 16.96 12.58 17.3 11.97L20.88 5.5C20.95 5.34 21 5.17 21 5C21 4.73478 20.8946 4.48043 20.7071 4.29289C20.5196 4.10536 20.2652 4 20 4H5.21L4.27 2M7 18C5.89 18 5 18.89 5 20C5 20.5304 5.21071 21.0391 5.58579 21.4142C5.96086 21.7893 6.46957 22 7 22C7.53043 22 8.03914 21.7893 8.41421 21.4142C8.78929 21.0391 9 20.5304 9 20C9 19.4696 8.78929 18.9609 8.41421 18.5858C8.03914 18.2107 7.53043 18 7 18Z" fill="#156E3E" />
-                      </svg>
-                      <p className="ms-3 text-[18px] text-slicing-foreground poppins-bold">
-
-                        Masukan Keranjang</p>
-                    </div>
-
-
                   </div>
                 </div>
               </div>
               <div>
-                <div className="max-w-sm bg-white border border-gray-200 rounded shadow">
-                  <a href="#">
-                    <img className="rounded-t bg-slicing-deep" src="/images/slicing/cookingTools.png" alt="" />
-                  </a>
+                <div className="shadow-md rounded  ">
+                  <img className=" bg-slicing-deep rounded-t" src="/images/slicing/redBag.png" alt="" />
                   <div className="p-5">
-
-                    <p className='text-[20px] poppins-bold'>Nesting</p>
-                    <p className="text-[18px] text-slicing-grey poppins-regular mt-2">Rp. 7.000 <span className='text-end'> / hari</span></p>
+                    <p className='text-[12px] lg:text-[14px]  xl:text-[20px] poppins-bold truncate'>Carrier 60L + Rain Cover</p>
+                    <p className="text-[12px] lg:text-[12px]  xl:text-[18px] text-slicing-grey poppins-regular mt-2">Rp 25.000 <span className='text-end'> / hari</span></p>
                     <div className='flex justify-center mt-2'>
                       <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M17 18C15.89 18 15 18.89 15 20C15 20.5304 15.2107 21.0391 15.5858 21.4142C15.9609 21.7893 16.4696 22 17 22C17.5304 22 18.0391 21.7893 18.4142 21.4142C18.7893 21.0391 19 20.5304 19 20C19 19.4696 18.7893 18.9609 18.4142 18.5858C18.0391 18.2107 17.5304 18 17 18ZM1 2V4H3L6.6 11.59L5.24 14.04C5.09 14.32 5 14.65 5 15C5 15.5304 5.21071 16.0391 5.58579 16.4142C5.96086 16.7893 6.46957 17 7 17H19V15H7.42C7.3537 15 7.29011 14.9737 7.24322 14.9268C7.19634 14.8799 7.17 14.8163 7.17 14.75C7.17 14.7 7.18 14.66 7.2 14.63L8.1 13H15.55C16.3 13 16.96 12.58 17.3 11.97L20.88 5.5C20.95 5.34 21 5.17 21 5C21 4.73478 20.8946 4.48043 20.7071 4.29289C20.5196 4.10536 20.2652 4 20 4H5.21L4.27 2M7 18C5.89 18 5 18.89 5 20C5 20.5304 5.21071 21.0391 5.58579 21.4142C5.96086 21.7893 6.46957 22 7 22C7.53043 22 8.03914 21.7893 8.41421 21.4142C8.78929 21.0391 9 20.5304 9 20C9 19.4696 8.78929 18.9609 8.41421 18.5858C8.03914 18.2107 7.53043 18 7 18Z" fill="#156E3E" />
                       </svg>
-                      <p className="ms-3 text-[18px] text-slicing-foreground poppins-bold">
+                      <p className="ms-3 text-[10px] lg:text-[12px]  xl:text-[18px]  text-slicing-foreground poppins-bold">
 
                         Masukan Keranjang</p>
                     </div>
-
-
                   </div>
                 </div>
               </div>
               <div>
-                <div className="max-w-sm bg-white border border-gray-200 rounded shadow">
-                  <a href="#">
-                    <img className="rounded-t bg-slicing-deep" src="/images/slicing/camp.png" alt="" />
-                  </a>
+                <div className="shadow-md rounded  ">
+                  <img className=" bg-slicing-deep rounded-t" src="/images/slicing/cookingTools.png" alt="" />
                   <div className="p-5">
-
-                    <p className='text-[20px] poppins-bold'>Tenda 4-5 Orang</p>
-                    <p className="text-[18px] text-slicing-grey poppins-regular mt-2">Rp 35.000 <span className='text-end'> / hari</span></p>
+                    <p className='text-[12px] lg:text-[14px]  xl:text-[20px] poppins-bold truncate'>Nesting</p>
+                    <p className="text-[12px] lg:text-[12px]  xl:text-[18px] text-slicing-grey poppins-regular mt-2">Rp 7.000 <span className='text-end'> / hari</span></p>
                     <div className='flex justify-center mt-2'>
                       <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M17 18C15.89 18 15 18.89 15 20C15 20.5304 15.2107 21.0391 15.5858 21.4142C15.9609 21.7893 16.4696 22 17 22C17.5304 22 18.0391 21.7893 18.4142 21.4142C18.7893 21.0391 19 20.5304 19 20C19 19.4696 18.7893 18.9609 18.4142 18.5858C18.0391 18.2107 17.5304 18 17 18ZM1 2V4H3L6.6 11.59L5.24 14.04C5.09 14.32 5 14.65 5 15C5 15.5304 5.21071 16.0391 5.58579 16.4142C5.96086 16.7893 6.46957 17 7 17H19V15H7.42C7.3537 15 7.29011 14.9737 7.24322 14.9268C7.19634 14.8799 7.17 14.8163 7.17 14.75C7.17 14.7 7.18 14.66 7.2 14.63L8.1 13H15.55C16.3 13 16.96 12.58 17.3 11.97L20.88 5.5C20.95 5.34 21 5.17 21 5C21 4.73478 20.8946 4.48043 20.7071 4.29289C20.5196 4.10536 20.2652 4 20 4H5.21L4.27 2M7 18C5.89 18 5 18.89 5 20C5 20.5304 5.21071 21.0391 5.58579 21.4142C5.96086 21.7893 6.46957 22 7 22C7.53043 22 8.03914 21.7893 8.41421 21.4142C8.78929 21.0391 9 20.5304 9 20C9 19.4696 8.78929 18.9609 8.41421 18.5858C8.03914 18.2107 7.53043 18 7 18Z" fill="#156E3E" />
                       </svg>
-                      <p className="ms-3 text-[18px] text-slicing-foreground poppins-bold">
+                      <p className="ms-3 text-[10px] lg:text-[12px]  xl:text-[18px]  text-slicing-foreground poppins-bold">
 
                         Masukan Keranjang</p>
                     </div>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <div className="shadow-md rounded  ">
+                  <img className=" bg-slicing-deep rounded-t" src="/images/slicing/camp.png" alt="" />
+                  <div className="p-5">
+                    <p className='text-[12px] lg:text-[14px]  xl:text-[20px] poppins-bold truncate'>Tenda 4-5 Orang</p>
+                    <p className="text-[12px] lg:text-[12px]  xl:text-[18px] text-slicing-grey poppins-regular mt-2">Rp 35.000 <span className='text-end'> / hari</span></p>
+                    <div className='flex justify-center mt-2'>
+                      <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M17 18C15.89 18 15 18.89 15 20C15 20.5304 15.2107 21.0391 15.5858 21.4142C15.9609 21.7893 16.4696 22 17 22C17.5304 22 18.0391 21.7893 18.4142 21.4142C18.7893 21.0391 19 20.5304 19 20C19 19.4696 18.7893 18.9609 18.4142 18.5858C18.0391 18.2107 17.5304 18 17 18ZM1 2V4H3L6.6 11.59L5.24 14.04C5.09 14.32 5 14.65 5 15C5 15.5304 5.21071 16.0391 5.58579 16.4142C5.96086 16.7893 6.46957 17 7 17H19V15H7.42C7.3537 15 7.29011 14.9737 7.24322 14.9268C7.19634 14.8799 7.17 14.8163 7.17 14.75C7.17 14.7 7.18 14.66 7.2 14.63L8.1 13H15.55C16.3 13 16.96 12.58 17.3 11.97L20.88 5.5C20.95 5.34 21 5.17 21 5C21 4.73478 20.8946 4.48043 20.7071 4.29289C20.5196 4.10536 20.2652 4 20 4H5.21L4.27 2M7 18C5.89 18 5 18.89 5 20C5 20.5304 5.21071 21.0391 5.58579 21.4142C5.96086 21.7893 6.46957 22 7 22C7.53043 22 8.03914 21.7893 8.41421 21.4142C8.78929 21.0391 9 20.5304 9 20C9 19.4696 8.78929 18.9609 8.41421 18.5858C8.03914 18.2107 7.53043 18 7 18Z" fill="#156E3E" />
+                      </svg>
+                      <p className="ms-3 text-[10px] lg:text-[12px]  xl:text-[18px]  text-slicing-foreground poppins-bold">
 
-
+                        Masukan Keranjang</p>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -529,7 +525,7 @@ function Home() {
           </div>
         </div>
       </div>
-      
+
     </div >
   )
 }
